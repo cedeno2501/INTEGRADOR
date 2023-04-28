041e23c1fcd142e349f3becd88004a1ec189e0ae
prueba <128333323+cedeno2501@users.noreply.github.com>
2023-04-28 00:07:50 -0300

    script

---
+++ futbol_jesus.sql
@@ -0,0 +1,11 @@
+drop table equipos;
+use futbol;
+insert into equipos_argentina values (1, "pelucas", 8, 4, "jose lee", 3,3);
+insert into equipos_argentina values (2, "bechica", 8, 4, "luis caña", 3,3);
+insert into equipos_argentina values (3, "famosos", 8, 4, "diego pal", 2,3);
+insert into equipos_argentina values (4, "rapidos", 8, 4, "jose maria", 1,3);
+insert into equipos_argentina values (5, "peluche", 8, 4, "jesus can", 2,3);
+
+select * from equipos_argentina;
+delete from equipos_argentina;
+
